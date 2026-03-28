d914d0d740ccd354854b3a3e4f883469e318cc75
shezroy1 <44572192+shezroy1@users.noreply.github.com>
2026-03-28 19:17:00 -0400

    connect orders controller and router and make changes as specified in my tests so that my tests are passing

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,173 @@
 const path = require("path");
 
-// Use the existing order data
+// Use the existing orders data
 const orders = require(path.resolve("src/data/orders-data"));
 
-// Use this function to assigh ID's when necessary
+// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+const orderExists = (req, res, next) => {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+
+  if (foundOrder) {
+    res.locals.order = foundOrder;
+    return next();
+  }
+
+  next({ status: 404, message: `Order id ${orderId} not found.` });
+};
+
+const bodyHasProperty = (propertyName) => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+
+    if (propertyName === "status") {
+      if (
+        !data[propertyName] ||
+        !["pending", "preparing", "in-progress", "ready"].includes(
+          data[propertyName],
+        )
+      ) {
+        return next({
+          status: 400,
+          message: `Order status must be one of: pending, preparing, in-progress, ready`,
+        });
+      }
+    }
+
+    if (data[propertyName]) {
+      return next();
+    }
+
+    next({
+      status: 400,
+      message: `Order property ${propertyName} not found.`,
+    });
+  };
+};
+
+const bodyHasDishes = () => {
+  return (req, res, next) => {
+    const { data: { dishes } = {} } = req.body;
+    if (!Array.isArray(dishes) || dishes.length === 0) {
+      return next({
+        status: 400,
+        message: "Order must include at least one dish",
+      });
+    }
+
+    for (let i = 0; i < dishes.length; i++) {
+      const dish = dishes[i];
+      if (
+        !dish.quantity ||
+        typeof dish.quantity !== "number" ||
+        dish.quantity <= 0
+      ) {
+        return next({
+          status: 400,
+          message: `Dish ${i} must have a quantity that is an integer greater than 0`,
+        });
+      }
+    }
+
+    return next();
+  };
+};
+
+const bodyHasId = () => {
+  return (req, res, next) => {
+    const { data: { id } = {} } = req.body;
+
+    if (!Boolean(id) || id === res.locals.order.id) {
+      return next();
+    }
+
+    next({
+      status: 400,
+      message: `id $${id} not valid.`,
+    });
+  };
+};
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+const list = (req, res) => {
+  res.json({ data: orders });
+};
+
+const read = (req, res) => {
+  const foundOrder = res.locals.order;
+
+  res.json({ data: foundOrder });
+};
+
+const create = (req, res) => {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  const newOrder = {
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+    id: nextId(),
+  };
+
+  orders.push(newOrder);
+
+  res.status(201).json({ data: newOrder });
+};
+
+const destroy = (req, res, next) => {
+  const order = res.locals.order;
+
+  if (order.status !== "pending") {
+    return next({
+      status: 400,
+      message: `Order id ${order.id} is pending.`,
+    });
+  }
+
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+
+  const deletedOrder = orders.splice(index, 1);
+  res.sendStatus(204);
+};
+
+const update = (req, res, next) => {
+  const order = res.locals.order;
+  const { data: { id, deliverTo, mobileNumber, status, dishes } = {} } =
+    req.body;
+
+  order.deliverTo = deliverTo;
+  order.mobileNumber = mobileNumber;
+  order.status = status;
+  order.dishes = dishes;
+
+  if (id) {
+    order.id = id;
+  }
+
+  res.json({ data: order });
+};
+
+module.exports = {
+  list,
+  read: [orderExists, read],
+  create: [
+    bodyHasProperty("deliverTo"),
+    bodyHasProperty("mobileNumber"),
+    bodyHasDishes(),
+    create,
+  ],
+  destroy: [orderExists, destroy],
+  update: [
+    orderExists,
+    bodyHasId(),
+    bodyHasProperty("deliverTo"),
+    bodyHasProperty("mobileNumber"),
+    bodyHasProperty("status"),
+    bodyHasDishes(),
+    update,
+  ],
+};
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const ordersController = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router
+  .route("/:orderId")
+  .get(ordersController.read)
+  .delete(ordersController.destroy)
+  .put(ordersController.update)
+  .all(methodNotAllowed);
+router
+  .route("/")
+  .get(ordersController.list)
+  .post(ordersController.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
